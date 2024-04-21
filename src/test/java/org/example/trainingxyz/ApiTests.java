0733049746070cc6d545e8dacab48c1fa88c4ceb
https://github.com/AytbaevErlan <irlanaytbaev@gmail.com>
2024-04-21 04:25:18 +0200

    verifying response header

--- src/test/java/org/example/trainingxyz/ApiTests.java
+++ src/test/java/org/example/trainingxyz/ApiTests.java
@@ -5,6 +5,7 @@ import org.example.models.Product;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class ApiTests {
 
@@ -18,13 +19,18 @@ public class ApiTests {
     @Test
     public void getProduct() {
         String endpoint = "http://localhost:8888/api_testing/product/read_one.php";
-        var response =
                 given().queryParam("id", 2).
                         when().
                         get(endpoint).
-                        then();
+                        then().
+                        assertThat().statusCode(200).body("id", equalTo("2")).
+                body("name", equalTo("Cross-Back Training Tank")).
+                        body("description", equalTo("The most awesome phone of 2013!")).
+                        body("price", equalTo("299.00")).
+                        body("category_id", equalTo("2")).
+                        body("category_name", equalTo("Active Wear - Women"));
 
-        response.log().body();
+
 
     }
 
@@ -140,6 +146,22 @@ public class ApiTests {
         );
         var respnose = given().body(product).when().post(endpoint).then();
         respnose.log().body();
+    }
+
+    @Test
+    public void getProducts(){
+        String endpoint = "http://localhost:8888/api_testing/product/read.php";
+        given().when().get(endpoint).then().log().headers().assertThat().statusCode(200).
+                headers("Content-Type", equalTo("application/json; charset=UTF-8")).
+                body("records.size()", greaterThan(0)).
+                body("records.id", everyItem(notNullValue())).
+                body("records.name", everyItem(notNullValue())).
+                body("records.description", everyItem(notNullValue())).
+                body("records.price", everyItem(notNullValue())).
+                body("records.category_id", everyItem(notNullValue())).
+                body("records.category_name", everyItem(notNullValue())).
+                body("records.id[0]", equalTo("1004"));
+
     }
 
 }
